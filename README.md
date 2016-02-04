f40d21cb89615cad2a3101b70668568b5fcde871
Nikhil Patil <nikhilpatil123@gmail.com>
2016-02-04 16:15:37 +0530

    readme changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Simple One Click Subtitle Downloader
+-----------------------
+-----------------------
+
+Just For Linux Users
+
+##Nemo file manager
+ * Place file nemo.sh in ~/.gnome2/nemo-scripts and also change the path for download_subtitle as per required
+ * Also make sure that script has permissions to allow it execute as a program.
+     You can do it by going to 'Properties → Permissions → Allow executing file as program' or
+
+
--- download_subtitle.py
+++ download_subtitle.py
@@ -1,19 +1,19 @@
-import hashlib
 import os
+import hashlib
 import sys
 
-PY_VERSION = sys.version_info[0]
+TYPE = sys.version_info[0]
 #Added support for both versions of python
-if PY_VERSION == 3:
+if TYPE == 3:
     import urllib.request
-if PY_VERSION == 2:
+if TYPE == 2:
     import urllib2
 
 
 #receives the name of the file and returns the hash code (Cpopied from documentation :P)
-def get_hash(file_path):
+def get_hash(req_path):
     read_size = 64 * 1024
-    with open(file_path, 'rb') as f:
+    with open(req_path, 'rb') as f:
         data = f.read(read_size)
         f.seek(-read_size, os.SEEK_END)
         data += f.read(read_size)
@@ -22,37 +22,35 @@ def get_hash(file_path):
 
 #think of more possible extensions are possible
 def is_valid_extention(extension):
-    possible_extensions=[".avi", ".mp4", ".mkv", ".mpg", ".mpeg", ".mov", ".rm", ".vob", ".wmv", ".flv", ".3gp",".3g2"]
+    possible_extensions=[ ".mp4",".avi", ".mkv",".flv",".mpg", ".mpeg", ".mov", ".vob", ".wmv",".3gp",".3g2"]
     if extension not in possible_extensions:
         return True
     else :
         return False
 
-def download_subtitle(file_path):
+def download_subtitle(req_path):
     try:
-        root, extension = os.path.splitext(file_path)
+        op_path, extension = os.path.splitext(req_path)
         if is_valid_extention(extension):
             return 
 
-        if not os.path.exists(root + ".srt"):
+        if not os.path.exists(op_path + ".srt"):
             headers = {'User-Agent': 'SubDB/1.0 (download_subtitle/1.0; http://github.com/nick9999/download_subtitle.git)'}
-            url = "http://api.thesubdb.com/?action=download&hash=" + get_hash(file_path) + "&language=en"
-            if PY_VERSION == 3:
+            url = "http://api.thesubdb.com/?action=download&hash=" + get_hash(req_path) + "&language=en"
+            if TYPE == 3:
                 req = urllib.request.Request(url, None, headers)
                 response = urllib.request.urlopen(req).read()
-            if PY_VERSION == 2:
+            if TYPE == 2:
                 req = urllib2.Request(url, '', headers)
                 response = urllib2.urlopen(req).read()
 
-            with open(root + ".srt", "wb") as subtitle:
+            with open(op_path + ".srt", "wb") as subtitle:
                 subtitle.write(response)
     except:
-        print("Could not get subtitle for " + file_path)
+        print("Could not get subtitle for " + req_path)
 
 
 def main():
-    root, _ = os.path.splitext(sys.argv[0])
-
     if len(sys.argv) == 1:
         print("This program requires at least one parameter")
         sys.exit(1)
@@ -61,12 +59,14 @@ def main():
 
         if os.path.isdir(path):
             # if this is a directory then download subtitles for each file 
-            for dir_path, _, file_names in os.walk(path):
+            for directory_path, _, file_names in os.walk(path):
                 for filename in file_names:
-                    file_path = os.path.join(dir_path, filename)
-                    download_subtitle(file_path)
+                    req_path = os.path.join(directory_path, filename)
+                    download_subtitle(req_path)
         else:
             download_subtitle(path)
+
+
 
 if __name__ == '__main__':
     main()
